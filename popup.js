c86310c4605ca8d9d05c9d5caea7128dd9698c89
jung.sammie <jung.sammie@gmail.com>
2020-10-23 23:53:01 +0900

    Alarm setting panel design is changed, edit/delete bookmark functions are removed.

--- popup.js
+++ popup.js
@@ -1,32 +1,27 @@
-$(function() {
-   //load bookmarkBoard
-   dumpBookmarks();
-  
+window.addEventListener('DOMContentLoaded', (event) => {
   //load alarmBoard
-	getAlarmCnt(function(count) {
+	getAlarmCnt(count => {
 		if(count !== 0) {
+			$('.row_end__item').show();
 			let alarmList = getBookmarkAlarms();
 			$('#alarmcontent').append(alarmList);
-			$('#alarmBoardTopRow').show();
-		} else {
-			$('#alarmBoardTopRow').hide();
-		}
+    } 
   });
   
   //search a bookmark
-	$('#searchBar').change(function() {
-    $('#bookmarkBoard').empty();
-    $('.inner').show();
-		dumpBookmarks($('#searchBar').val());
-	}); 
+	$('#searchBar').change(() => {
+      $('#bookmarkBoard').empty();
+      $('.inner').show();
+      dumpBookmarks($('#searchBar').val());
+  }); 
 
   //clear an alarm
-	$('#alarmList').off('click').on('click', '.clear', function() {
+	$('#alarmList').off('click').on('click', '.clear', function(){
 		let alarmItem = $(this).parent(),
         link = alarmItem[0].classList[0],
         isLeft = true;
         
-		getAlarmCnt(function(count) {
+		getAlarmCnt(count => {
       count === 0 ? 0: count--;
 			let notificationCnt = count.toString();
 			if(notificationCnt === '0') {
@@ -36,47 +31,51 @@ $(function() {
 			else 
 				chrome.browserAction.setBadgeText({text: notificationCnt});
 			
-			chrome.alarms.clear(link, function(wasCleared) {
-				if(wasCleared) {
+			chrome.alarms.clear(link, (wasCleared) => {
+        if (wasCleared) {
           alarmItem.remove();
-					if(!isLeft) { 
-            $('#alarmBoardTopRow').hide();
+          if (!isLeft) {
+            $('.row_end__item').hide();
           }
-					alert('The bookmark alarm has removed successfully!');
-				}
-				else {
-					alert('44:Oops! Error has occured..');
-				}
-			});
+          alert('The bookmark alarm has removed successfully!');
+        }
+        else {
+          alert('44:Oops! Error has occured..');
+        }
+      });
 		});
 	});
 
   //clear all alarm
-	$('#alarmBoard').on('click', '#alarmBoardClear', function() {
+	$('#alarmBoard').on('click', '#alarmBoardClear', () => {
 		if(confirm('Are you sure clear all bookmark alarm?')) {
-			chrome.alarms.clearAll(function(wasCleared) {
-				if(wasCleared) {
-					// alert('All bookmark alarm is cleared!');
-					chrome.browserAction.setBadgeText({text: ''});
-				} else {
-					alert('57:Oops! Error has occured..');
-				}
-			});
+			chrome.alarms.clearAll((wasCleared) => {
+          if (wasCleared) {
+            // alert('All bookmark alarm is cleared!');
+            chrome.browserAction.setBadgeText({ text: '' });
+          } else {
+            alert('57:Oops! Error has occured..');
+          }
+        });
 			$('#alarmList').empty();
-			$('#alarmBoardTopRow').hide();
+      $('.row_end__item').hide();
 		}
 	});
 
-	chrome.storage.onChanged.addListener(function(changes, namespace) {
-		for(key in changes) {
-			chrome.storage.sync.get(key, function(result) {
-				if(result[key] === 'fired') {
-					let item = $('#'+removeSpecialChar(key));
-					item.remove();
-				}
-			});
-		}
-	});
+	chrome.storage.onChanged.addListener((changes, namespace) => {
+      for (key in changes) {
+        chrome.storage.sync.get(key, (result) => {
+          if (result[key] === 'fired') {
+            let item = $('#' + removeSpecialChar(key));
+            item.remove();
+            $('.row_end__item').hide();
+          }
+        });
+      }
+  }); 
+
+  //load bookmarkBoard
+  dumpBookmarks();
 }); //end of document.ready()
 
 /**
@@ -95,22 +94,22 @@ function getBookmarkAlarms(obj) {
                 + title 
                 + '<span class="clear"><img src="assets/clear.png" class="option_icon_md"></span>'
                 +'</li>'
-		$('#alarmBoardTopRow').show();
+    $('.row_end__item').show();
 		$alarmList.append(item);
 	} else {
-		chrome.alarms.getAll(function(Alarms) {
-			for(let i = 0 ; i < Alarms.length ; i++) {
-				let link = Alarms[i].name;
-				chrome.bookmarks.search(Alarms[i].name, function(BookmarkTreeNodes) {
-					let title = BookmarkTreeNodes[0].title,
-              item = '<li id="'+removeSpecialChar(link)+'" class="'+ link +' alarm_item">'
-                      + title 
-                      +'<span class="clear"><img src="assets/clear.png" class="option_icon_md"></span>'
-                      +'</li>';
-					$alarmList.append(item);
-				});	
-			}
-		});
+		chrome.alarms.getAll((Alarms) => {
+        for (let i = 0; i < Alarms.length; i++) {
+          let link = Alarms[i].name;
+          chrome.bookmarks.search(Alarms[i].name, (BookmarkTreeNodes) => {
+            let title = BookmarkTreeNodes[0].title,
+              item = '<li id="' + removeSpecialChar(link) + '" class="' + link + ' alarm_item">'
+                + title
+                + '<span class="clear"><img src="assets/clear.png" class="option_icon_md"></span>'
+                + '</li>';
+            $alarmList.append(item);
+          });
+        }
+      });
 	}
 	return $alarmList;
 }
@@ -182,14 +181,15 @@ function dumpNode(bookmarkNode, query) {
 
 	//When bookmark has a url, a bookmark. If not it's a title
 	if(!bookmarkNode.url) {
-		if(bookmarkNode.title === '') 
-		  $directories.removeClass('toggle');
+		if(bookmarkNode.title === '') {
+      $directories.removeClass('toggle');
+    }
 		$directories.text(bookmarkNode.title);
 		$span.append($directories);
 	} else {
     $bookmarkItem.attr('href', bookmarkNode.url);
     $bookmarkItem.text(bookmarkNode.title);
-		$bookmarkItem.click(function() {
+		$bookmarkItem.off('click').on('click', function() {
 			chrome.tabs.create({url: bookmarkNode.url});
 		});
 		$span.append($bookmarkItem);
@@ -201,97 +201,82 @@ function dumpNode(bookmarkNode, query) {
 	}
 
 	/*When hovered item is a folder, show add button 
-	 *when it's a bookmark, show edit and delete button*/
-	let $options = bookmarkNode.children ?
+	 *when it's a bookmark, show alarm button*/
+  let $options = bookmarkNode.children ?
 		$('<span><span id="addBtn" class="option_btn"><img src="assets/add.png" class="option_icon_md"></span></span>') :
-		$('<span><span id="editBtn" class="option_btn"><img src="assets/edit.png" class="option_icon_md"></span> ' +
-		'<span id="deleteBtn" class="option_btn"><img src="assets/delete.png" class="option_icon_md"></span> '+
-		'<span id="alarmBtn" class="option_btn"><img src="assets/alarm.png" class="option_icon_md"></span></span>');
+		$('<span id="alarmBtn" class="option_btn"><img src="assets/alarm.png" class="option_icon_md"></span></span>');
 	
   let $alarmOptions = $('<div class="setAlarmPanel">'+
-      '<input type="radio" id="test" name="alarmterm" value="0.1" checked> <label for="0.5min">test</label> ' +
-			'<input type="radio" id="5min" name="alarmterm" value="5" > <label for="5min">5min</label>' +
-			'<input type="radio" id="15min" name="alarmterm" value="15"> <label for="15min">15min</label> ' +
-			'<input type="radio" id="30min" name="alarmterm" value="30"> <label for="30min">30min</label><br> ' +
-			'<input type="submit" id="setAlarm" class="btn" value="SET"></div>');
+    '<div id="radioBtns">' +
+    '<input type="radio" id="test" name="alarmterm" value="0.1" checked> <label for="0.5min">test</label> <br/>' +
+    '<input type="radio" id="5min" name="alarmterm" value="5" > <label for="5min">5min</label> <br/>' +
+    '<input type="radio" id="15min" name="alarmterm" value="15"> <label for="15min">15min</label> <br/>' +
+    '<input type="radio" id="30min" name="alarmterm" value="30"> <label for="30min">30min</label><br> ' +
+    '</div> <div id="setBtn">' +
+    '<input type="submit" id="setAlarm" class="btn" value="SET"></div></div>');
       
-	$span.hover(function() {
+	$span.hover(function () {
     $span.append($options);
-		$('#deleteBtn').click(function() {
-			if(confirm('Are you sure want to delete this bookmark?')) {
-				chrome.bookmarks.remove(String(bookmarkNode.id));
-				$span.parent().remove(); 
-			} 
-		});//end of deleteBtn click
 
-		$('#editBtn').click(function() {
-			let title = $bookmarkItem.text();
-			let editedTitle = prompt('Edit Title',title);
-			if(editedTitle !== null && editedTitle !== title) {
-				chrome.bookmarks.update(String(bookmarkNode.id), {
-					title: editedTitle
-				});
-				$bookmarkItem.text(editedTitle);
-			}
-		});//end of editBtn click
+    $('#alarmBtn').off('click').on('click', function () {
+        let title = $bookmarkItem.text(),
+          link = $bookmarkItem[0].href;
+        $span.append($alarmOptions);
+        $('#setAlarm').click(() => {
+          chrome.alarms.get(link, (alarm) => {
+            if (alarm !== undefined) {
+              alert('The same alarm is already on you list.');
+            } else {
+              getAlarmCnt((count) => {
+                count++;
+                let notificationCnt = count.toString(),
+                  alarmTerm = $('input[name=alarmterm]:checked').val(),
+                  minutes = parseFloat(alarmTerm);
+                chrome.browserAction.setBadgeText({ text: notificationCnt });
+                chrome.alarms.create(link, { delayInMinutes: minutes });
+                chrome.storage.sync.set({ minutes: minutes });
+                alert('You \'ll get alarmed in ' + minutes + 'min about ' + title);
+                getBookmarkAlarms({ alarmTitle: title, alarmLink: link });
+              });
+              chrome.storage.sync.set({ [removeSpecialChar(link)]: 'set' });
+            }
+          });
+        });
+      }); //end of alarmBtn click
 
-		$('#alarmBtn').on('click', function(event) {
-			let title = $bookmarkItem.text(),
-			    link = $bookmarkItem[0].href;
-			$span.append($alarmOptions);
-			$('#setAlarm').click(function() {
-				chrome.alarms.get(link, function(alarm) {
-					if(alarm !== undefined) {
-						alert('The same alarm is already on you list.');
-					} else {
-						getAlarmCnt(function(count) {
-							count++;
-							let notificationCnt = count.toString(),
-							    alarmTerm = $('input[name=alarmterm]:checked').val(),
-							    minutes = parseFloat(alarmTerm);
-							chrome.browserAction.setBadgeText({text: notificationCnt});
-							chrome.alarms.create(link, {delayInMinutes: minutes});
-							chrome.storage.sync.set({minutes: minutes});
-							alert('You \'ll get alarmed in ' +minutes + 'min about ' + title);
-							getBookmarkAlarms({alarmTitle: title, alarmLink: link});
-						});
-						chrome.storage.sync.set({[removeSpecialChar(link)]: 'set'});
-					}
-				});
-			});
-		});//end of alarmBtn click
+    $('#addBtn').off('click').on('click', function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+          let currentTab = tabs[0],
+            title = currentTab.title,
+            url = currentTab.url;
+          chrome.bookmarks.create({ parentId: bookmarkNode.id, title: title, url: url }, function (result) {
+            if (result.url === url && result.title === title) {
+              alert('New bookmark has added!');
+              $('#bookmarkBoard').empty();
+              dumpBookmarks();
+            } else {
+              alert('268:Oops! Error has occured..');
+            }
+          });
+        });
+      }); //end of addBtn click
 
-		$('#addBtn').click(function() {
-			chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-				let currentTab = tabs[0],
-				    title = currentTab.title,
-				    url = currentTab.url;
-				chrome.bookmarks.create({parentId: bookmarkNode.id, title: title, url: url}, function(result) {
-					if(result.url === url && result.title === title) {
-						alert('New bookmark has added!');
-						$('#bookmarkBoard').empty();
-						dumpBookmarks();
-					} else {
-						alert('268:Oops! Error has occured..');
-					}
-				});
-			});
-    });//end of addBtn click
-
-    $('.toggle').off('click').on('click', function(event) {
+    $('.toggle').off('click').on('click', function () {
+      console.log('clicked');
       let $this = $(this).parent();
-      console.log($this);
-      if($this.next().hasClass('show')) { //where ul class is inner
+    
+      if ($this.next().hasClass('show')) { //where ul class is inner
         $this.next().removeClass('show');
-        $this.next().slideUp(500);
+        $this.next().slideUp();
       } else {
         $this.parent().parent().find('li .inner').removeClass('show');
-        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.parent().parent().find('li .inner').slideUp();
         $this.next().toggleClass('show');
         $this.next().slideToggle();
+        
       }
     });
-	}, 
+  }, 
 	//unhover
 	function() {
 		$options.remove();
@@ -308,5 +293,5 @@ function dumpNode(bookmarkNode, query) {
  * @param <function> callback
  */
 function getAlarmCnt(callback) {
-    chrome.alarms.getAll(function(alarms) { callback(alarms.length) }); 
+    chrome.alarms.getAll(alarms => {callback(alarms.length) }); 
 }
